cdc67399cf98b3eba2d0d69ddcbb895b743eb137
GOPIKA V <397159@manappuram.com>
2025-05-22 10:31:45 +0530

    Initial commit

---
+++ task/Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace task.Model
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string qualification { get; set; }
+        public int salary { get; set; }
+    }
+}
